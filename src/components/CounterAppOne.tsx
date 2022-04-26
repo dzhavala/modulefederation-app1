9af727830cfdeb5036262c42f5659b06b380ebe9
dzhavala <dzhavala@gmail.com>
2022-04-26 17:26:11 +0300

    add event bus support

--- src/components/CounterAppOne.tsx
+++ src/components/CounterAppOne.tsx
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
-import './CounterAppOne.css';
+import React, { useState, useEffect } from "react";
+import "@trutoo/event-bus";
+import "./CounterAppOne.css";
+import { ChannelEvent } from "@trutoo/event-bus/dist/event-bus";
 
-type Count = number
+type Count = number;
 
 const Counter = () => {
   const [count, setCount] = useState<Count>(0);
 
+  useEffect(() => {
+    function handleSubscribe(countEvent: ChannelEvent<number>) {
+      setCount(countEvent.payload!);
+    }
+    eventBus.register("count:set", { type: "number" });
+    const sub = eventBus.subscribe<number>("count:set", handleSubscribe);
+    return function cleanup() {
+      sub.unsubscribe();
+    };
+  }, []);
+
+  function setCountGlobaly() {
+    eventBus.publish("count:set", count + 1);
+  }
+
   return (
-    <div className='CounterAppOne'>
+    <div className="CounterAppOne">
       <p>Your click count : {count} </p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={setCountGlobaly}>Click me</button>
     </div>
   );
 };
